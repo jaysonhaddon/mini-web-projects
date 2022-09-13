41991a3750df7b90f4ce1beaefa49d3ad74a6d58
jaysonhaddon <jayson.haddon@outlook.com>
2022-09-12 20:40:05 -0600

    Added Blurry Loading Project

---
+++ blurry-loading/script.js
@@ -0,0 +1,24 @@
+const loadText = document.querySelector(".loading-text");
+const background = document.querySelector(".bg");
+const footer = document.querySelector("footer");
+
+console.log(footer);
+
+let load = 0;
+
+let interval = setInterval(blurring, 30);
+
+function blurring() {
+  load++;
+  if (load > 99) {
+    footer.removeAttribute("hidden");
+    clearInterval(interval);
+  }
+  loadText.textContent = `${load}%`;
+  loadText.style.opacity = scale(load, 0, 100, 1, 0);
+  background.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+}
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+};
